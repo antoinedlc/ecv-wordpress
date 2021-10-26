278fdea70a5e92994a95eebd78e4b68659a6e724
Quentin <quentin.moras@gmail.com>
2021-10-26 12:16:24 +0300

    Article add

--- wp-content/themes/ecv-travel-blog/functions.php
+++ wp-content/themes/ecv-travel-blog/functions.php
@@ -5,11 +5,3 @@ add_theme_support( 'post-thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
-
-//lien avec la feuille de style 
-function theme_scripts()
-{
-    wp_enqueue_style("theme-style", get_stylesheet_uri());
-    wp_enqueue_script('main-script', get_template_directory_uri() . '/js/script.js', array('jquery'));
-}
-add_action("wp_enqueue_scripts", "theme_scripts");
